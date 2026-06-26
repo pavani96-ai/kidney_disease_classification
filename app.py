c12e3aa7a889c5d759bf208f77ec40024a0b47bc
pavani <pavanikusuma2020@gmail.com>
2026-06-26 04:50:50 +0000

    completed data pipeline versioning with dvc

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from flask_cors import CORS, cross_origin
+from cnnClassifier.utils.common import decodeImage
+from cnnClassifier.pipeline.prediction import PredictionPipeline
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.png"
+
+clApp = ClientApp()
+
+
+
+@app.route("/", methods= ['GET'])
+@cross_origin()
+def home():
+    return render_template("index.html")
+
+
+@app.route("/train" , methods =["GET",'POST'])
+@cross_origin()
+def trainRoute():
+    os.system("dvc repro")
+    return "Trainin Done Succesfully!"
+
+@app.route("/predict", methods=['Post'])
+@cross_origin()
+def predictionRoute():
+    image_data = request.json['image']
+    decodeImage(image_data , clApp.filename)
+    classifier = PredictionPipeline(clApp.filename)
+    output = classifier.predict()
+    return jsonify(output)
+
+if __name__ == "__main__":
+   
+    app.run(host='0.0.0.0', port=8000)
+
---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,27 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename = filename
+        self.model = load_model("artifacts/training/model.keras")
+        
+        
+    def predict(self):
+        image_name = self.filename
+        test_image = image.load_img(image_name, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        predictions =self.model.predict(test_image)
+        result = np.argmax(predictions, axis=1)[0]
+        print(f"Predicted class index: {result}")
+
+        classes = ["Cyst", "Normal", "Stone", "Tumor"]
+        
+        predicted_label = classes[result]
+
+        return [{"image": predicted_label}]
+
+       
